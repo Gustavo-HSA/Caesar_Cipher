526e745d759c596c421132ec47a1ddb14326cc36
Gustavo <xdrules1995@gmail.com>
2022-06-22 14:44:29 -0300

    Bug fixes and improvements

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import art
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z']
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 print(art.logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 
 def caesar(start_text, shift_amount, choosen_direction):
     end_text = ""
     if choosen_direction == 'decode':
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
     print(f"The {choosen_direction}d text is {end_text}")
 
 
-caesar(start_text=text, shift_amount=shift, choosen_direction=direction)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, choosen_direction=direction)
+
+result = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n").lower
+if result == 'no':
+    should_continue = False
+    print("Goodbye")
